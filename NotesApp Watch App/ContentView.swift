7712540793ea5011375f8d03c3df43ea0f0a83e8
karim <kariimAhmed8@gmail.com>
2024-09-11 21:11:39 +0300

    Initial Project files

---
+++ NotesApp Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotesApp Watch App
+//
+//  Created by Karim Soliman on 10/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Herr kariim8!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotesApp Watch App/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp Watch App
+//
+//  Created by Karim Soliman on 10/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
